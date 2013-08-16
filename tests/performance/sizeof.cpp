eaf36ad9a802e2fb0af17e34a231fd3444cc1641
John Biddiscombe <biddisco@cscs.ch>
2013-08-16 09:20:36 +0200

    Fix a couple of compile problems on windows
    Conflicts:
    	hpx/async.hpp
    	tests/performance/sizeof.cpp

--- tests/performance/sizeof.cpp
+++ tests/performance/sizeof.cpp
@@ -2,6 +2,7 @@
 //
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+#define WIN32_LEAN_AND_MEAN
 
 #include <hpx/hpx_init.hpp>
 #include <hpx/include/iostreams.hpp>
