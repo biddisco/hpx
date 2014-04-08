d91f8a4de58b0ec53752c18f3058c25d08dec9dc
John Biddiscombe <biddisco@cscs.ch>
2014-04-08 22:56:19 +0200

    Windows build fixes for JB

--- hpx/util/backtrace/backtrace.hpp
+++ hpx/util/backtrace/backtrace.hpp
@@ -23,7 +23,7 @@
 // if they want just this one to be dynamically linked:
 #if defined(BOOST_ALL_DYN_LINK) || defined(HPX_BACKTRACE_DYN_LINK)
 // export if this is our own source, otherwise import:
-#ifdef HPX_BACKTRACE_SOURCE
+#ifdef hpx_EXPORTS
 # define HPX_BACKTRACE_DECL BOOST_SYMBOL_EXPORT
 #else
 # define HPX_BACKTRACE_DECL BOOST_SYMBOL_IMPORT
