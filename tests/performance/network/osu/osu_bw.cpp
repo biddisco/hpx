9f7edc70d6b78ce826f469cd276cb6aec2eb4a71
John Biddiscombe <biddisco@cscs.ch>
2016-11-29 11:38:15 +0100

    WIP: OSU benchmark cleanup

--- tests/performance/network/osu/osu_bw.cpp
+++ tests/performance/network/osu/osu_bw.cpp
@@ -11,43 +11,61 @@
 #include <hpx/include/serialization.hpp>
 #include <hpx/include/parallel_for_each.hpp>
 #include <hpx/include/util.hpp>
+#include <hpx/util/detail/yield_k.hpp>
 
-#include <boost/scoped_array.hpp>
+// Includes
+#ifdef HPX_PARCELPORT_VERBS_HAVE_LOGGING
+#  include <plugins/parcelport/verbs/rdma/rdma_logging.hpp>
+#else
+#  define LOG_DEBUG_MSG(x)
+#endif
+//
 #include <boost/range/irange.hpp>
-
+//
 #include <cstddef>
 #include <cstdint>
 #include <cstring>
 #include <vector>
 
 ///////////////////////////////////////////////////////////////////////////////
-char* align_buffer (char* ptr, unsigned long align_size)
-{
-    return (char*)(((std::size_t)ptr + (align_size - 1)) / align_size * align_size);
-}
-
-#if defined(HPX_WINDOWS)
-unsigned long getpagesize()
-{
-    SYSTEM_INFO si;
-    GetSystemInfo(&si);
-    return si.dwPageSize;
-}
-#endif
+#define LOOP_SMALL_MULTIPLIER 5
+#define SKIP                  20
+#define LARGE_MESSAGE_SIZE    8192
+#define MAX_MSG_SIZE         (1<<22)
 
 ///////////////////////////////////////////////////////////////////////////////
-#define LOOP_SMALL_MULTIPLIER 5
-#define SKIP  2
-
-#define LARGE_MESSAGE_SIZE  8192
-
-#define MAX_MSG_SIZE (1<<22)
-#define MAX_ALIGNMENT 65536
-
+typedef hpx::serialization::serialize_buffer<char> buffer_type;
+// used to track messages that have been 'sent'
+std::atomic<std::size_t> count;
 
 ///////////////////////////////////////////////////////////////////////////////
 void isend(hpx::serialization::serialize_buffer<char> const& receive_buffer) {}
-HPX_PLAIN_ACTION(isend);
+HPX_PLAIN_DIRECT_ACTION(isend);
+
+///////////////////////////////////////////////////////////////////////////////
+void async_callback(std::atomic<std::size_t> &count, boost::system::error_code
+    const& ec, hpx::parcelset::parcel const& p)
+{
+    // this global atomic tracks how many messages have 'been sent'
+    count++;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+void one_thread_sender(hpx::naming::id_type dest, std::size_t iterations, buffer_type send_buffer)
+{
+    LOG_DEBUG_MSG("Thread sender " << decnumber(iterations));
+    using hpx::util::placeholders::_1;
+    using hpx::util::placeholders::_2;
+    //
+    isend_action send;
+    for (std::size_t i=0; i<iterations; ++i) {
+        hpx::apply_cb(send, dest,
+            hpx::util::bind(&async_callback,
+                std::ref(count), _1, _2),
+            send_buffer);
+    }
+    //LOG_DEBUG_MSG("Done Thread sender " << decnumber(iterations));
+}
 
 ///////////////////////////////////////////////////////////////////////////////
 double ireceive(hpx::naming::id_type dest, std::size_t loop,
@@ -60,41 +78,62 @@ double ireceive(hpx::naming::id_type dest, std::size_t loop,
         skip *= LOOP_SMALL_MULTIPLIER;
     }
 
-    typedef hpx::serialization::serialize_buffer<char> buffer_type;
+    // if we only have 1 thread, then we must suspend when waiting to allow
+    // the network to progress, if we have >1 threads, we can use a very short delay
+    // in yield_k that will not trigger a suspension
+    std::size_t threads = hpx::get_os_thread_count();
+    int suspend_delay = 24;
+    if (threads>1) {
+        suspend_delay = 4;
+    }
+    //LOG_DEBUG_MSG("Running with " << threads << " threads")
 
-    // align used buffers on page boundaries
-    unsigned long align_size = getpagesize();
-    (void)align_size;
-    HPX_ASSERT(align_size <= MAX_ALIGNMENT);
+    typedef std::vector<char> char_buffer;
+    std::vector<char_buffer> send_buffers(threads, char_buffer(size));
 
-    boost::scoped_array<char> send_buffer(new char[size]);
-    std::memset(send_buffer.get(), 'a', size);
-
-    hpx::util::high_resolution_timer t;
-
-    isend_action send;
-    for (std::size_t i = 0; i != loop + skip; ++i) {
-        // do not measure warm up phase
-        if (i == skip)
-            t.restart();
-
-        using hpx::parallel::for_each;
-        using hpx::parallel::execution::par;
-
-        std::size_t const start = 0;
-
-        auto range = boost::irange(start, window_size);
-        for_each(par, boost::begin(range), boost::end(range),
-            [&](std::uint64_t j)
-            {
-                send(dest, buffer_type(
-                    send_buffer.get(), size, buffer_type::reference));
-            }
-        );
+    for(std::size_t i=0; i<threads;++i) {
+        std::memset(send_buffers[i].data(), 'a', size);
     }
 
+    using namespace hpx::parallel;
+
+    // create one task per OS thread and launch messages from those worker tasks
+    count = 0;
+    std::size_t zero = 0;
+    std::size_t iterations =  loop/threads;
+    auto range = boost::irange(zero, threads);
+    for_each(par.with(static_chunk_size(1)), boost::begin(range), boost::end(range),
+        [&](std::size_t j)
+        {
+            one_thread_sender(dest, skip,
+                buffer_type(send_buffers[j].data(), size, buffer_type::reference));
+        }
+    );
+    // wait until the last message triggers the callback, so we know all msgs are sent
+    while (count < skip*threads) {
+        // this makes the cpu wait with without suspending our thread
+        hpx::util::detail::yield_k(24, nullptr);
+    }
+
+    count = 0;
+    hpx::util::high_resolution_timer t;
+    for_each(par.with(hpx::parallel::static_chunk_size(1)), boost::begin(range), boost::end(range),
+        [&](std::size_t j)
+        {
+            one_thread_sender(dest, iterations,
+                buffer_type(send_buffers[j].data(), size, buffer_type::reference));
+        }
+    );
+    LOG_DEBUG_MSG("Here with count = " << count);
+    // wait until the last message triggers the callback, so we know all msgs are sent
+    while (count < iterations*threads) {
+        // this makes the cpu wait briefly with without suspending our thread
+        hpx::util::detail::yield_k(24, nullptr);
+    }
+    LOG_DEBUG_MSG("Here with count = " << count);
+
     double elapsed = t.elapsed();
-    return (size / 1e6 * loop * window_size) / elapsed;
+    return (size / 1e6 * iterations*threads) / elapsed;
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -109,6 +148,7 @@ void run_benchmark(boost::program_options::variables_map & vm)
 {
     // use the first remote locality to bounce messages, if possible
     hpx::id_type here = hpx::find_here();
+    uint64_t     rank = hpx::naming::get_locality_id_from_id(here);
 
     hpx::id_type there = here;
     std::vector<hpx::id_type> localities = hpx::find_remote_localities();
@@ -122,8 +162,10 @@ void run_benchmark(boost::program_options::variables_map & vm)
     // perform actual measurements
     for (std::size_t size = min_size; size <= max_size; size *= 2)
     {
-        double bw = ireceive(there, loop, size, vm["window-size"].as<std::size_t>());
-        hpx::cout << std::left << std::setw(10) << size
-                  << bw << hpx::endl << hpx::flush;
+        if (rank==0) {
+            double bw = ireceive(there, loop, size, vm["window-size"].as<std::size_t>());
+            hpx::cout << std::left << std::setw(10)
+                << size << bw << hpx::endl << hpx::flush;
+        }
     }
 }
