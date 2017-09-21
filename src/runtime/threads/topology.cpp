c64265c9920d1f8af8943e29bb3d4b1676c6d7c4
John Biddiscombe <biddisco@cscs.ch>
2017-09-21 13:45:03 +0200

    WIP: adding some thread priority checks

--- src/runtime/threads/topology.cpp
+++ src/runtime/threads/topology.cpp
@@ -14,6 +14,7 @@
 #include <hpx/util/static.hpp>
 
 #include <cstddef>
+#include <inttypes.h>
 
 #if defined(__ANDROID__) && defined(ANDROID)
 #include <cpu-features.h>
@@ -70,6 +71,40 @@ namespace hpx { namespace threads
         return (!any(res)) ? machine_mask : res;
     }
 
+    void print_rlimit(struct rlimit *r, const char *name)
+    {
+        int64_t cur;                /* Soft limit */
+        int64_t max;                /* Hard limit */
+        cur = r->rlim_cur;
+        max = r->rlim_max;
+        printf("RLIMIT_%s\t:rlim_cur => %" PRId64
+                        ", :rlim_max => %" PRId64 "\n", name, cur, max);
+    }
+
+    void set_sched_param()
+    {
+        const char *sched_policy[] = {
+        "SCHED_OTHER",
+        "SCHED_FIFO",
+        "SCHED_RR",
+        "SCHED_BATCH"
+        };
+        struct sched_param sp = {
+            .sched_priority = 90
+        };
+        pid_t pid = getpid();
+        printf("pid=(%d)\n",pid);
+        sched_setscheduler(pid, SCHED_RR, &sp);
+        printf("Scheduler Policy is %s.\n", sched_policy[sched_getscheduler(pid)]);
+
+//        pthread_t tid ;
+//        pthread_create(&tid , NULL, Thread1 , (void*)(long)3);
+//        pthread_create(&tid , NULL, Thread2 , (void*)(long)3);
+//        pthread_create(&tid , NULL, Thread3 , (void*)(long)3);
+//        while(1)
+//            sleep(100);
+    }
+
     bool topology::set_mempolicy(mask_cref_type cpu_mask, error_code& ec) const
     {
         set_mem_policy_interleaved(cpu_mask, ec);
@@ -78,6 +113,24 @@ namespace hpx { namespace threads
 
     bool topology::reduce_thread_priority(error_code& ec) const
     {
+
+//        set_sched_param();
+/*
+        struct rlimit rlim;
+        int resources[] = {RLIMIT_CORE, RLIMIT_CPU, RLIMIT_DATA, RLIMIT_FSIZE,
+                           RLIMIT_MEMLOCK, RLIMIT_NOFILE, RLIMIT_NPROC, RLIMIT_RSS,
+                           RLIMIT_STACK, RLIMIT_NICE};
+        const char *names[] = {"CORE", "CPU", "DATA", "FSIZE",
+                               "MEMLOCK", "NOFILE", "NPROC", "RSS",
+                               "STACK", "NICE"};
+        int n = sizeof(resources)/sizeof(resources[0]);
+        int i;
+        for (i = 0; i < n; i++) {
+            getrlimit(resources[i], &rlim);
+            print_rlimit(&rlim, names[i]);
+        }
+*/
+
 #ifdef HPX_HAVE_NICE_THREADLEVEL
 #if defined(__linux__) && !defined(__ANDROID__) && !defined(__bgq__)
         pid_t tid;
