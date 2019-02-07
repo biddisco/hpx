d0d8837a45e526e2e129ac109c3db366bfa9a7cb
John Biddiscombe <biddisco@cscs.ch>
2019-02-07 16:42:43 +0100

    Merge with 07169901c46a8bcd49cdc895bb272a9cdd1217ed

--- hpx/parallel/executors/thread_execution.hpp
+++ hpx/parallel/executors/thread_execution.hpp
@@ -133,7 +133,6 @@ namespace hpx { namespace threads
             util::deferred_call(std::forward<F>(f), std::forward<Ts>(ts)...),
             "hpx::parallel::execution::post",
             threads::pending,
-            true,
             exec.get_stacksize(),
             std::forward<Hint>(hint),
             throws);
